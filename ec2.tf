087c80203004c2be4bbac4a64c1659282c9fc288
sahmofor <59724047+sahmofor@users.noreply.github.com>
2022-12-08 01:07:04 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "app" {
     ami           = "ami-0b0dcb5067f052a63"
     instance_type = "t3.micro"
-    security_groups = ["jekSG"]
+    security_groups = ["nexus-SG"]
     key_name      =  "ubuntu.pem"
     tags = {
         Name = " tomcat"
