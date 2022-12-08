1e093efa5c343adf8345ba21abd019cdd3c8e162
sahmofor <59724047+sahmofor@users.noreply.github.com>
2022-12-08 01:04:33 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,8 @@ provider "aws" {
 resource "aws_instance" "app" {
     ami           = "ami-0b0dcb5067f052a63"
     instance_type = "t3.micro"
-    security_groups = ["jkSG"]
-    key_name      =  "key29"
+    security_groups = ["jekSG"]
+    key_name      =  "ubuntu.pem"
     tags = {
         Name = " tomcat"
         Environment = "dev"
